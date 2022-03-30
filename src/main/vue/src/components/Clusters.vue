50b23387ed2ed23a15f9ffffdf798d454a14e2b7
Vít Koma <vit.koma@memsource.com>
2022-03-30 21:26:36 +0200

    Allow reloading cluster list

--- src/main/vue/src/components/Clusters.vue
+++ src/main/vue/src/components/Clusters.vue
@@ -4,20 +4,44 @@ import http from "../services/http";
 export default {
   data() {
     return {
+      loading: true,
       clusters: []
     }
   },
   mounted() {
-    http
-        .get("/clusters")
-        .then(response => (this.clusters = response.data));
+    reload(this)
+  },
+  methods: {
+    reload() {
+      reload(this)
+    }
   }
 }
+
+const reload = (data) => {
+  data.loading = true;
+  http
+      .get("/clusters")
+      .then(response => {
+        data.clusters = response.data
+        data.loading = false
+      })
+}
+
+
 </script>
 
 <template>
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Clusters</h1>
+  </div>
+  <div class="pt-3">
+    <div class="text-start">
+      <button @click="reload" type="button" class="btn btn-primary me-2">
+        <span v-if="loading" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+        {{loading ? "Loading..." : "Reload"}}
+      </button>
+    </div>
   </div>
   <table class="table">
     <thead>
